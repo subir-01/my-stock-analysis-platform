56f611c24058422b6ffd41b9565b043eb37c6921
subir-01 <mydashboard06@gmail.com>
2026-08-25 01:31:03 +0530

    Initial Spring Boot project setup

---
+++ src/main/java/com/Dashboard/myTradingPlatform/MyTradingPlatformApplication.java
@@ -0,0 +1,13 @@
+package com.Dashboard.myTradingPlatform;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyTradingPlatformApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MyTradingPlatformApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/Dashboard/myTradingPlatform/MyTradingPlatformApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Dashboard.myTradingPlatform;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyTradingPlatformApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
